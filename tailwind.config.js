b40ed1833999a0d848e7b3836368f223877074e3
Akash Mehra <128696316+itsakki10@users.noreply.github.com>
2025-04-28 02:29:12 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./*.html", "./*.pcss"],
+  theme: {
+    extend: {
+      spacing: {
+        80: "20rem",
+        60: "15rem",
+      }
+    },
+  },
+  plugins: [],
+}
